adb9bf4144633e71b91a606d1697bfabd36b3c3b
Damiaan Dufaux <Damiaan.Dufaux@dolby.com>
2024-11-22 17:02:21 +0100

    add 3.18.1 rc1
    Make framework mergable

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "THEOliveSDK",
-            url: "https://cdn.theo.live/artifacts/apple/THEOlive-3.18.0.xcframework.zip",
-            checksum: "994c5ab0533eb102306cc59d66331b7b2daa1db648d631e526be56438b9cb074"
+            url: "https://cdn.theo.live/artifacts/apple/THEOlive-3.18.1-rc.1.xcframework.zip",
+            checksum: "9ede0bb60f402fd9e6ef49ce67c959dd7911eebc8961397a33557b74547413ec"
         ),
     ]
 )
