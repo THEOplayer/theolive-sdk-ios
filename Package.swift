998339b892db3ab69073d8660a113a138f84ee6a
Damiaan Dufaux <Damiaan.Dufaux@dolby.com>
2024-12-17 12:13:14 +0100

    add 3.18.6-rc.1
    adds background support

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "THEOliveSDK",
-            url: "https://cdn.theo.live/artifacts/apple/THEOlive-3.18.5.xcframework.zip",
+            url: "https://cdn.theo.live/artifacts/apple/development/THEOlive-3.18.6-rc.1.xcframework.zip",
             checksum: "7063370625f466f51227284b19d3f015364030a53211296abd41b6956dce6d1a"
         ),
     ]
